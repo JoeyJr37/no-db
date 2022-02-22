20f5885d9c3810ccacaa51aa28a2b96fab9693ca
Joey <Joey@antiochhouston.com>
2022-02-22 12:28:26 -0600

    Updated staffList.js to sort names alphabetically by last name

--- src/components/StaffList/StaffList.js
+++ src/components/StaffList/StaffList.js
@@ -2,8 +2,19 @@ import React from 'react';
 import StaffCard from '../StaffCard/StaffCard';
 
 const StaffList = ({ data, showEmployee }) => {
+
+    const nameSort = (a, b) => {
+        if (a.last_name > b.last_name){
+            return 1;
+        } else if (a.last_name < b.last_name){
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
     return (
-        data.map((employee, i) => {
+        data.sort(nameSort).map((employee, i) => {
             return <StaffCard key={i} data={employee} showEmployee={showEmployee}/>
         })
     )
